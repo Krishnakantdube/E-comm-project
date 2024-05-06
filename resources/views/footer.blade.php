4040c2e3285e2d796ee1893ace952c6d980cda88
krishnakant Dube <krishnakantdube0@gmail.com>
2024-05-07 02:29:32 +0530

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="pannel pannel-default">
+  <div class="pannel-body">
+    pannel content 
+
+  </div>
+  <div class="pannel-footer">pannel footer</div>
+
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">E-Comm</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li>
+        <a class="nav-link" href="#">orders <span class="sr-only"></span></a>
+      </li>
+      <form class="nav navbar-nav navbar-left">
+        <div class="form-group">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        </div>
+        <button type="submit" class="btn btn-default"> Submit</button>
+
+    </form>
+
+
+    <ul class="nav navbar-nav navbar-right">
+
+    <li><a href="#"> Add To Cart <a/></li>
+      
+      
+    </ul>
+  </div>
+</nav>
